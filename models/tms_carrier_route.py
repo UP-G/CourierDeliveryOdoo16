125f8c4ecacb33a429b115d1be5dcffee8186b64
e.gokh <e.gokh@tmtr.ru>
2023-07-18 11:40:27 +0500

    fixed carrier route

--- models/tms_carrier_route.py
+++ models/tms_carrier_route.py
@@ -7,5 +7,5 @@ class TmsCarrierRoute(models.Model):
     name = fields.Char(string='Name', tracking=True, required=True)
 
     carrier_id = fields.Many2one('tms.carrier', string='Carrier id')
-    carrier_driver_id = fields.Many2one('tms.carrier.driver', string='Carrier driver id')
+    driver_id = fields.Many2one('tms.carrier.driver', string='Carrier driver id')
     route_id = fields.Many2one('tms.route', string='Route id')
